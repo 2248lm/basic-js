663ae96f8cd24d41101dda296a075aab296d83c2
Lazzat Mukhametalinova <2248lm@gmail.com>
2022-01-31 16:56:17 +0600

    feat: solution Delete digit

--- src/st-delete-digit.js
+++ src/st-delete-digit.js
@@ -11,7 +11,28 @@ import { NotImplementedError } from '../extensions/index.js';
  * For n = 152, the output should be 52
  *
  */
-export default function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function deleteDigit(n) {
+  let numLength = String(n).length-1;
+  let numDigitBefore, numDigitAfter, numBefore, numAfter, numGeneral, numMax;
+
+  numDigitAfter = 10 ** numLength;
+  numMax = n % numDigitAfter;
+  numDigitBefore = numDigitAfter;
+
+  for (let k = 1; k < numLength; k++) {
+    numDigitAfter = Math.pow(10, (numLength - k));
+    numBefore = Math.floor(n / numDigitBefore);
+    numAfter = n % numDigitAfter;
+    numGeneral = Number(String(numBefore)+String(numAfter));
+    numDigitBefore = numDigitAfter;
+    if (numGeneral > numMax) {
+      numMax = numGeneral;
+    }
+  }
+
+  numGeneral = Math.floor(n / 10);
+  if (numGeneral > numMax) {
+    numMax = numGeneral;
+  }
+  return numMax;
 }
