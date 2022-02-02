f7baef2d78ab6ac726dd6177ba177e3ea4898e2f
Lazzat Mukhametalinova <2248lm@gmail.com>
2022-02-02 13:45:41 +0600

    feat: solution Encode line

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -10,7 +10,21 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine(str) {
+  let count = 1, cipher = [];
+  for (let k = 1; k < str.length+1; k++) {
+    if (str[k] === str[k-1]) {
+      count += 1;
+    }
+    else {
+      if (count === 1) {
+        cipher.push(str[k-1]);
+      }
+      else {
+        cipher.push(count + str[k-1]);
+      }
+      count = 1;
+    }
+  }
+  return cipher.join('');
 }
